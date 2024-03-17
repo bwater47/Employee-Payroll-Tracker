0fb80c996c0693de31ce95d14f1e8c77f0e2060b
B Water <brennw47@gmail.com>
2024-03-16 20:21:44 -0700

    Updated the script.js file to have an empty array on line 6, a while loop on line 7, and a console log beneath it on line 10.

--- Develop/script.js
+++ Develop/script.js
@@ -3,6 +3,11 @@ const addEmployeesBtn = document.querySelector('#add-employees-btn');
 
 // Collect employee data
 const collectEmployees = function() {
+  const Employees = []
+  while () {
+
+  }
+  console.log();
   // TODO: Get user input to create and return an array of employee objects
 }
 
